dc2382f5911c87305d11834fa91a1ebb5c055eba
Jesus De la Cruz <145614120+JoseDelacruzS@users.noreply.github.com>
2024-11-22 07:48:36 -0600

    Requerimientos

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\jjdel\Escritorio\7mo semestre\Sistemas Moviles\Proyecto\UTaskV02\Backend\venv
+set VIRTUAL_ENV=D:\jjdel\Escritorio\7mo semestre\Sistemas Moviles\Proyecto\Backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
